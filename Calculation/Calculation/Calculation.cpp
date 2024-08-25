88297239a3bc55a3dcec9ef6ac913f21a5bb5f87
Hasta-Vista <ovchinnikov675@yandex.com>
2024-08-26 00:27:57 +0400

    dynamic

---
+++ Calculation/Calculation/Calculation.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <queue>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+int main() {
+    int number;
+    cin >> number;
+    queue<pair<int, vector<int>>> steps;
+    unordered_set<int> allX;
+    steps.push({ 1, {1} });
+    allX.insert(1);
+    while (!steps.empty()) {
+        int middle = steps.front().first;
+        vector<int> way = steps.front().second;
+        steps.pop();
+        int nextNumber[] = { middle + 1, middle * 2, middle * 3 };
+        for (int x : nextNumber) {
+            if (x == number) {
+                way.push_back(x);
+                cout << way.size() - 1 << endl;
+                for (int elem : way) {
+                    cout << elem << " ";
+                }
+                return 0;
+            }
+            if (x < number && allX.find(x) == allX.end()) {
+                vector<int>newWay = way;
+                newWay.push_back(x);
+                steps.push({ x, newWay });
+                allX.insert(x);
+
+            }
+        }
+    }
+    return 0;
+}
---
+++ LCS/LCS/LCS.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+    int firstSize;
+    cin >> firstSize;
+    vector<int> firstSequence(firstSize);
+    for (int i = 0; i < firstSize; ++i) {
+        cin >> firstSequence[i];
+    }
+    int secondSize;
+    cin >> secondSize;
+    vector<int> secondSequence(secondSize);
+    for (int i = 0; i < secondSize; ++i) {
+        cin >> secondSequence[i];
+    }
+    vector<vector<int>> result(firstSize + 1, vector<int>(secondSize + 1, 0));
+    for (int i = 1; i <= firstSize; ++i) {
+        for (int j = 1; j <= secondSize; ++j) {
+            if (firstSequence[i - 1] == secondSequence[j - 1]) {
+                result[i][j] = result[i - 1][j - 1] + 1;
+
+            }
+            else {
+                result[i][j] = max(result[i - 1][j], result[i][j - 1]);
+            }
+        }
+    }
+    cout << result[firstSize][secondSize] << endl;
+    return 0;
+}
